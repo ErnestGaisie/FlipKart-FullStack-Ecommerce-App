f660d0ce608b0f58d1e578e41f643abd77350244
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-07-06 13:13:32 +0000

    Api Request Validation done

--- mern-back-end/src/controllers/auth.js
+++ mern-back-end/src/controllers/auth.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
 
 exports.signup = (req, res) => {
-  const errors = validationResult(req);
-  return res.status(400).json({ errors: errors.array() });
+  // const errors = validationResult(req);
+  // return res.status(400).json({ errors: errors.array() });
 
   User.findOne({ email: req.body.email }).exec((error, user) => {
     if (user)
--- mern-back-end/src/routes/auth.js
+++ mern-back-end/src/routes/auth.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const { signup, signin, requireSignin } = require("../controllers/auth");
-const { check } = require("express-validator");
+
+const {
+  validateSigninRequest,
+  validateSignupRequest,
+  isRequestValidated,
+} = require("../validators/auth");
 const router = express.Router();
 
-router.post(
-  "/signup",
-  [
-    check("firstName").notEmpty().withMessage("firstName is required"),
-    check("lastName").notEmpty().withMessage("lastName is required"),
-    check("lastName"),
-    check("email").isEmail().withMessage("Valid Email is required"),
-    check("password")
-      .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 character long"),
-  ],
-  signup
-);
-router.post("/signin", signin);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 
 // router.post('/profile', requireSignin, (req, res) => {
 //     res.status(200).json({ user: 'profile'})
